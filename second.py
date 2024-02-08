a98ef0a9bc9af55e062fab2a06c91897b3925ed7
nisharamanna555 <nisha.ramanna.20@gmail.com>
2024-02-08 11:32:46 -0500

    added info abt nisha

--- second.py
+++ second.py
@@ -1,1 +1,6 @@
 print("This is the second file.")
+
+# git status -> which branch you're working on
+# git branch "name" -> creates new branch
+# git branch -l -> shows all branches
+# git checkout "name" -> switches to branch
